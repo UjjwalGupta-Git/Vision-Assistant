4dd5a683d2b02ac14d35001df3ce0684089b4dc0
Ujjwal Gupta <miscvecstor@gmail.com>
2021-07-22 22:20:27 +0530

    Easy Access to all possible phrases

---
+++ src/possiblePhrases.py
@@ -0,0 +1,10 @@
+possibleJokeSentences = {
+    "tell a joke",
+    "tell me a joke",
+    "tell a funny joke"
+    "tell joke",
+    "say a joke",
+    "say a funny joke",
+    "crack a joke",
+    "crack me up"
+}
