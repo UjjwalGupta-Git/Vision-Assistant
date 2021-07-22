4ebef1b3531b0540c41a281f84eb090001e2f392
Ujjwal Gupta <miscvecstor@gmail.com>
2021-07-22 21:36:11 +0530

    Removed pyttsx3, added gtts and playsound

--- scripts/MacOS/install_dependencies.sh
+++ scripts/MacOS/install_dependencies.sh
@@ -6,7 +6,8 @@
 brew install portaudio
 pip3 install pyaudio
 pip3 install SpeechRecognition
-pip3 install pyttsx3
 pip3 install requests
 pip3 install wikipedia
 pip3 install pydictionary
+pip3 install gtts
+pip3 install playsound
